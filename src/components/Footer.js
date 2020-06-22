52b9e3344c471708b215e1ca7d40df841bb293b6
csslick <digitone@naver.com>
2020-06-22 21:04:34 +0900

    sns icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,6 +8,11 @@ export const Footer = ()=>{
   return (
     <>
       <footer className="container" id="main_footer">
+        <ul className="sns">
+          <li><a href="https://cafe.naver.com/csslick"><img style={{marginTop:'2px'}} width="28px" src="./images/naver_cafe.png" title="naver cafe" alt="naver cafe"/></a></li>
+          <li><a href="https://github.com/csslick"><i class="fa fa-github-square" aria-hidden="true"></i></a></li>
+          <li><a href="mailto:tailofmoon@naver.com"><i class="fa fa-envelope" aria-hidden="true"></i></a></li>
+        </ul>
         <a href="#visual" className="btn_top">top</a>
         <small>&copy; {year} CSSLICK MEDIA. all rights reserved.</small>
       </footer>
